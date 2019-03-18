eff544940bc07cba616c12f5dd5b4797bcc1a2dd
iamnambiar <neeraj.cnambiar@gmail.com>
2019-03-19 01:01:11 +0530

    Located the corners in the image

--- area_algorithm.py
+++ area_algorithm.py
@@ -2,9 +2,14 @@ import cv2
 import numpy as np
 
 
-def converter():        # To convert image from BGR to Binary format
-    image = cv2.imread('Room.png', 0)
-    #cv2.imshow('ROOM', image)
-    ret, thresh = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
-    #cv2.imshow('ROOM THRESHOLD', thresh)
-    cv2.imwrite('Room_binary.jpg', thresh)
+gray_image = cv2.imread('Room.png', 0)
+row, height = gray_image.shape
+#cv2.imshow('ROOM', image)
+ret, binary = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)   # To convert image from Grayscale to Binary format
+#cv2.imshow('ROOM THRESHOLD', thresh)
+cv2.imwrite('Room_binary.jpg', binary)
+
+
+binary = np.float32(binary)
+corners = cv2.goodFeaturesToTrack(binary, 100, 0.01, 5)         #To find the corners of the image
+corners = np.int0(corners)
